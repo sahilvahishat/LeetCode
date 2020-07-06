05043e2ef09b7a9f21875079cb83d63135824233
Sahil Vashishat <sahilvashishat@Sahils-MacBook-Air.local>
2020-07-06 11:17:07 -0400

    updated package

--- Solution.java
+++ Solution.java
@@ -1,5 +1,3 @@
-package LeetCode;
-
 public class Solution {
     public static void main(String[] args) {
         // Object
--- kfreq.java
+++ kfreq.java
@@ -1,5 +1,3 @@
-package LeetCode;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
